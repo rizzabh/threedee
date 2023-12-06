f768cfdbfd83883c52b4ad703046f8daab652999
rizzabh <pandeyrishabh966@gmail.com>
2023-12-06 23:29:02 +0530

    2nd obj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <>
     <Spline className='mb-5' scene="https://prod.spline.design/Hx-Zy8XO8VBm7pPz/scene.splinecode" />
+    <Spline className='-mt-64 max-md:-ml-96 max-sm:-ml-[36rem]' scene="https://prod.spline.design/xW3wXWYHwSrrfMxL/scene.splinecode" />
 
 
     </>
